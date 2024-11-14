625f7c9c907cc30b918ba263abda3e8a58583a85
KostsPlayer <azka.nuril070@gmail.com>
2024-11-15 01:07:56 +0700

    set environment for makanear login

---
+++ src/pages/login/fetchLogin.js
@@ -0,0 +1,60 @@
+import { backend } from "../../helpers/urlConfig";
+
+export default function fetchLogin() {
+  const getEmail = document.querySelector("input[name='Email']");
+  const getPassword = document.querySelector("input[name='Password']");
+
+  document.querySelector(".login-form").addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const datajson = {
+      Email: getEmail.value,
+      Password: getPassword.value,
+    };
+
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    const raw = JSON.stringify(datajson);
+
+    const requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+    };
+
+    console.log(raw);
+
+    fetch(backend.login, requestOptions)
+      .then(async (response) => {
+        const status = response.status;
+        const result = await response.json();
+
+        if (status === 200) {
+          Swal.fire({
+            title: "Login Berhasil",
+            text: "Anda berhasil Login.",
+            icon: "success",
+          }).then((result) => {
+            if (result.isConfirmed) {
+              window.location.href = "/dashboard";
+            }
+          });
+        } else {
+          Swal.fire({
+            icon: "error",
+            title: "Gagal Login",
+            text: "Email atau Password tidak cocok",
+          });
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: "Something went wrong!",
+        });
+      });
+  });
+}
---
+++ src/pages/login/login.js
@@ -0,0 +1,17 @@
+import loadComponent from "/src/helpers/loadComponent.js";
+import { smoothScroll } from "/src/helpers/smoothScroll.js";
+import { url } from "/src/helpers/urlConfig.js";
+import fetchLogin from "/src/pages/login/fetchLogin.js";
+
+export default function main() {
+  const promise = loadComponent("", url.pages.login + "login.html");
+
+  promise
+    .then(() => {
+      smoothScroll();
+      fetchLogin();
+    })
+    .catch((error) => {
+      console.error("Error loading components:", error);
+    });
+}
